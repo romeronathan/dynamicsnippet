abe0438096448d1a2f988a553bf0d7fd4b2e89a9
nathan <romeronathan@hotmail.com>
2022-09-05 18:09:50 -0400

    Setup mongo and added some models

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    MONGODB_URI: "mongodb+srv://nathantest:nathantest@dynamicsnippets.zjcalkt.mongodb.net/?retryWrites=true&w=majority",
+    MONGODB_DB: "DynamicSnippets"
+  }
 }
 
 module.exports = nextConfig
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../middleware/database";
 
-type Data = {
-  name: string
-}
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest,res: NextApiResponse) {
+
+
+  const {db} = await connectToDatabase();
+  const collection = await db.collection("snippet").find({}).toArray();
+
+  res.json(collection);
+
 }
